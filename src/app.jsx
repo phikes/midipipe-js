627fe547e50d608f2dae21e7aef8a035bcafba9a
Phillip Kessels <info@phikes.de>
2023-06-21 10:14:13 +0100

    chore: use react 17 jsx transform, fix deprecation in react-dom

--- src/app.jsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-
-const render = () => {
-  ReactDOM.render(<h2>Hello from React!</h2>, document.body);
-}
-
-render();
---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import { createRoot } from 'react-dom/client';
+
+import { App } from './components/App/index.jsx'
+
+const domNode = document.getElementById('root');
+const root = createRoot(domNode);
+root.render(<App />);
